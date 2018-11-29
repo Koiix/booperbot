24455ff9118cfa7246b28d6b846463c7ae7dc4cd
Joel Kreider <kreiderj@purdue.edu>
2018-11-29 15:00:52 -0500

    more string functionality

--- app/app.js
+++ app/app.js
@@ -5,25 +5,38 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+function deleteSendBooper(msg){
+	msg.delete()
+		.then(msg.channel.send('Booper'));
+}
+
 client.on('message', msg => {
+  console.log(`**Got Message: ${msg.content} from user ${msg.author.username} ${msg.author.id}`);
+  if(msg.author.equals(client.user)){
+  	console.log(`	>Message created by bot`);
+  	return;
+  }
   let msg_str = msg.content.toLowerCase();
   if(msg.embeds.length > 0){
-  	msg.delete()
-  		.then(message.channel.send('Booper'));
-  		return;
+  	console.log(`    >Message includes embedded content`);
+  	deleteSendBooper(msg);
   } 
   let words = msg_str.split(" ");
+  console.log(`	   >Checking Message Content`);
   for (word in words) {
-  	let tempWord = word;
-
+  	let tempWord = words[word];
   	//replace special characters
   	tempWord = tempWord.replace("0", "o");
   	tempWord = tempWord.replace("3", "e");
   	tempword = tempWord.replace(":b:", "b");
 
-  	let boopers = word.split("booper");
-  	if(word.length > (boopers.length-1) * 6){
-  		
+  	let boopers = tempWord.split("booper");
+  	console.log(`		>>word: ${tempWord}`);
+  	console.log(`			>>>word length: ${tempWord.length}`);
+  	console.log(`			>>>booper allocated length: ${(boopers.length-1) * 6}`);
+  	if(tempWord.length > (boopers.length-1) * 6){
+  		console.log(`	>Message includes more than boopers`);
+  		deleteSendBooper(msg);
   	}
   }
 });
